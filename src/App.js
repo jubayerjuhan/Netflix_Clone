02be7e071efe2cb433a7e855f5b3939c15b8fa37
jubayerjuhan <davidjuhan23@gmail.com>
2021-08-31 12:11:56 +0600

    navbar added with flexbox

--- src/App.js
+++ src/App.js
@@ -6,20 +6,23 @@ import Navbar from './Components/Navbar/Navbar.js';
 
 function App() {
   return (
-    <div className="app">
+    <>
       <Navbar />
-      <Banner />
-      <div className="row-container">
-        <Row title="NETFLIX ORIGINALS" isLarge={true} fetchUrl={requests.fetchNetflixOriginals} />
-        <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
-        <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
-        <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
-        <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
-        <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
-        <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
-        <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+      <div className="app">
+        {/* <Navbar /> */}
+        <Banner />
+        <div className="row-container">
+          <Row title="NETFLIX ORIGINALS" isLarge={true} fetchUrl={requests.fetchNetflixOriginals} />
+          <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+          <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+          <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+          <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+          <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+          <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+          <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,9 +1,28 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import './Navbar.css'
 const Navbar = () => {
-  return (
-    <div>
+  const [show, handleShow] = useState(false);
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 90) {
+        handleShow(true);
+      } else handleShow(false);
 
+      return () => {
+        window.removeEventListener('scroll');
+      }
+    })
+  }, [])
+  return (
+    <div className={`nav ${show && 'navBlackBg'}`}>
+      <div className="nav-container">
+        <img className='nav-logo'
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png" alt="NetFlix Logo" />
+
+        <img className='nav-avatar'
+          src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+          alt="Netflx Avatar" />
+      </div>
     </div>
   )
 }
